3a5454330bf1a1808b3c1b80fcf04ff1739f7c75
Tina <oge612@gmail.com>
2025-08-23 23:48:49 +0200

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,26 @@
-import "./globals.css";
- 
+import Header from '@/components/Header';
+import './globals.css';
+
+import localFont from 'next/font/local';
+import classNames from 'classnames';
+
+const bb = localFont({
+  src: [
+    {
+      path: '../public/assets/fonts/BB-Regular.woff2',
+    },
+  ],
+  variable: '--font-bb',
+});
+
+const lUlys = localFont({
+  src: [
+    {
+      path: '../public/assets/fonts/lyno/lyno-ulys_2-webfont.woff2',
+    },
+  ],
+  variable: '--font-lulys',
+});
 
 export default function RootLayout({
   children,
@@ -7,11 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body >
+    <html lang='en'>
+      <body className={classNames(lUlys.variable, bb.variable)}>
+        <Header />
+
         {children}
       </body>
     </html>
   );
 }
- 
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import classNames from 'classnames';
+
+export default function Header() {
+  return (
+    <header className={classNames('ulys', 'header')}>
+      <h1>NOUMENON</h1>
+    </header>
+  );
+}
